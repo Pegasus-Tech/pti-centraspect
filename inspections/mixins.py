430bf28e8e0b8475bbf3ac850085f3a6c3c81eb1
Justin Dodson <justin.dodson@agoradata.com>
2022-03-18 15:23:44 -0500

    api error handling and testing

--- inspections/mixins.py
+++ inspections/mixins.py
@@ -1,24 +1,30 @@
 from .models import Inspection
 from datetime import date, timedelta
 
+from centraspect import messages
 import json
+
 
 class LogInspectionMixin:
 
     def log_inspection(self, *args, **kwargs):
         """
             When an inspection is logged, we need to update the related inspection item
-            to have the updated inpsection date, next due date, and then save the created 
+            to have the updated inspection date, next due date, and then save the created
             inspection log
         """
         rtn = {"success": False, "inspection": None}
 
-        item = kwargs.get('inspection_item')
-        user = kwargs.get('logged_by')
-        json_form = kwargs.get('completed_form')
-        disposition = kwargs.get('inspection_disposition')
+        (is_valid,
+         item,
+         user,
+         json_form,
+         disposition,
+         rtn
+         ) = self.__validate_inspection_log_params(**kwargs, rtn=rtn)
 
-        print(f"Got JSON form :: {json_form}")
+        if not is_valid:
+            return rtn
 
         try:
             inspection = Inspection.objects.create(
@@ -33,7 +39,10 @@ class LogInspectionMixin:
             rtn['inspection'] = inspection
             rtn['success'] = True
         except Exception as e:
-            print(e)
+            print(f"Error logging inspection from API :: {str(e)}")
+            rtn['success'] = False
+            rtn['inspection'] = None
+            rtn['error'] = str(e)
         finally:
             return rtn
 
@@ -88,3 +97,32 @@ class LogInspectionMixin:
     def is_failed_inspection(self, disposition):
         print(f'DISPOSITION :: {disposition}')
         return disposition == 'fail'
+
+    def __validate_inspection_log_params(self, params, rtn):
+        is_valid = True
+        item = params.get('inspection_item') or None
+        user = params.get('logged_by') or None
+        json_form = params.get('completed_form') or None
+        disposition = params.get('inspection_disposition') or None
+
+        print(f"Got JSON form :: {json_form}")
+
+        if item is None:
+            rtn['error'] = messages.NO_INSPECTION_ITEM_PROVIDE
+            is_valid = False
+            return is_valid, item, user, json_form, disposition, rtn
+        if user is None:
+            rtn['error'] = messages.NO_LOGGED_BY_USER_PROVIDED
+            is_valid = False
+            return is_valid, item, user, json_form, disposition, rtn
+        if json_form is None:
+            rtn['error'] = messages.NO_FORM_ATTACHED_ERROR
+            is_valid = False
+            return is_valid, item, user, json_form, disposition, rtn
+        if disposition is None:
+            rtn['error'] = messages.NO_INSPECTION_DISPOSITION_PROVIDED
+            is_valid = False
+            return is_valid, item, user, json_form, disposition, rtn
+
+        return is_valid, item, user, json_form, disposition, rtn
+
--- inspections/views.py
+++ inspections/views.py
@@ -6,7 +6,6 @@ from inspection_items.models import InspectionItem
 from .mixins import LogInspectionMixin
 import json
 
-# Create your views here.
 
 class LogNewInspectionView(LoginRequiredMixin, LogInspectionMixin, View):
     
