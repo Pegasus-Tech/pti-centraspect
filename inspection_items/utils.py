fef5850ecd6e45d353915195a4acce959a019469
Justin Dodson <justindodson@Justins-Air.attlocal.net>
2022-03-20 20:19:48 -0500

    simplify qr code data encoding

--- inspection_items/utils.py
+++ inspection_items/utils.py
@@ -4,13 +4,7 @@ import json
 def serialize_inspection_item(instance):
     return json.dumps(
         {
-            "uuid": instance.uuid.__str__(), 
-            "date_created": instance.date_created.__str__(),
-            "serial_number": instance.serial_number, 
-            "model_number": instance.model_number,
-            "expiration_date": instance.expiration_date.__str__(),
-            "form_uuid": instance.form.uuid.__str__() if instance.form is not None else None, 
-            "assigned_to_id": instance.assigned_to.pk if instance.assigned_to is not None else None, 
-            "account_uuid": instance.account.uuid.__str__()
+            "uuid": instance.uuid.__str__(),
+            "form_uuid": instance.form.uuid.__str__() if instance.form is not None else None
         }
     )
